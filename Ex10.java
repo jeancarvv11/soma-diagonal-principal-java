afe61513420c622c6f5bd9792c2647808ef28d34
Jean Carlo <jeancarlo.carvalho@hotmail.com>
2026-05-25 19:39:09 -0300

    Add files via upload

---
+++ Ex10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Ex10 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int [][] m =  {
+            {1, 2, 3, 4,},
+            {2, 3, 4, 5,},
+            {3, 4, 5, 6,},
+            {4, 5, 6, 7,}};
+
+        for ( int i = 0; i < m.length; i++){
+            for ( int j = 0; j < m[i].length; j++){
+                System.out.print("[" + m[i][j] + "]");
+            }
+            System.out.println();
+        }
+
+        int soma = 0;
+        for ( int i = 0; i < m.length; i++){
+            for ( int j = 0; j < m[i].length;j++){
+                if ( i == j){
+                soma = soma + m[i][j];
+                }
+            }
+        }
+        System.out.println("soma dos elementos abaixo da diag principal: " + soma);
+    }
+}
